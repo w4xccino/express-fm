ad6db376204763d76ba791092894b96c42c216c0
Diego Toledo <dbtoledoa@gmail.com>
2022-11-16 04:49:33 -0500

    add: change some html

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,38 +61,25 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Radio Express FM <p className="text-color-primary">98.9</p>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Con la musica de tu mejor epoca!
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
-                    </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
+                  <Button tag="a" color="primary" wideMobile href="https://www.fmexpres.com/">
+                    Ir al sitio web!
                     </Button>
                 </ButtonGroup>
               </div>
             </div>
           </div>
-          <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
-            <a
-              data-video="https://player.vimeo.com/video/174002812"
-              href="#0"
-              aria-controls="video-modal"
-              onClick={openModal}
-            >
-              <Image
-                className="has-shadow"
-                src={require('./../../assets/images/video-placeholder.jpg')}
-                alt="Hero"
-                width={896}
-                height={504} />
-            </a>
+          <div>
+            <video controls autoplay name="media" poster="http://www.fmexpres.com/wp-content/uploads/2020/12/Logo-4.jpg">
+              <source src="http://20.254.104.74:8000/prueba" type="audio/aac"/>
+            </video>
           </div>
           <Modal
             id="video-modal"
