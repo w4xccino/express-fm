777434e44fd3f6bfaacb31db2682e51d0795b57b
Diego Toledo <dbtoledoa@gmail.com>
2022-11-27 09:41:37 -0500

    feat: add logo and radio player

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import ButtonGroup from '../elements/ButtonGroup';
-import Button from '../elements/Button';
-import Image from '../elements/Image';
-import Modal from '../elements/Modal';
+import React, { useState } from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import ButtonGroup from "../elements/ButtonGroup";
+import Button from "../elements/Button";
+// import Image from "../elements/Image";
+import Modal from "../elements/Modal";
+import RadioPlayer from "./RadioPlayer";
 
 const propTypes = {
-  ...SectionProps.types
-}
+  ...SectionProps.types,
+};
 
 const defaultProps = {
-  ...SectionProps.defaults
-}
+  ...SectionProps.defaults,
+};
 
 const Hero = ({
   className,
@@ -24,74 +25,84 @@ const Hero = ({
   invertColor,
   ...props
 }) => {
-
   const [videoModalActive, setVideomodalactive] = useState(false);
 
   const openModal = (e) => {
     e.preventDefault();
     setVideomodalactive(true);
-  }
+  };
 
   const closeModal = (e) => {
     e.preventDefault();
     setVideomodalactive(false);
-  }   
+  };
 
   const outerClasses = classNames(
-    'hero section center-content',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
+    "hero section center-content",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
     className
   );
 
   const innerClasses = classNames(
-    'hero-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
+    "hero-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
   );
 
   return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
+    <section {...props} className={outerClasses}>
       <div className="container-sm">
         <div className={innerClasses}>
           <div className="hero-content">
-            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+            <h1
+              className="mt-0 mb-16 reveal-from-bottom"
+              data-reveal-delay="200"
+            >
               Radio Express FM <p className="text-color-primary">98.9</p>
             </h1>
             <div className="container-xs">
-              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+              <p
+                className="m-0 mb-32 reveal-from-bottom"
+                data-reveal-delay="400"
+              >
                 Con la musica de tu mejor epoca!
-                </p>
+              </p>
+              <div>
+                <RadioPlayer />
+              </div>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://www.fmexpres.com/">
+                  <Button
+                    tag="a"
+                    color="primary"
+                    wideMobile
+                    href="https://www.fmexpres.com/"
+                  >
                     Ir al sitio web!
-                    </Button>
+                  </Button>
                 </ButtonGroup>
               </div>
             </div>
           </div>
+          <div></div>
           <div>
-            <video controls autoplay name="media" poster="http://www.fmexpres.com/wp-content/uploads/2020/12/Logo-4.jpg">
-              <source src="http://20.254.104.74:8000/live" type="audio/aac"/>
+            <video
+              controls
+              autoplay
+              name="media"
+              poster="http://www.fmexpres.com/wp-content/uploads/2020/12/Logo-4.jpg"
+            >
+              <source src="http://20.254.104.74:8000/live" type="audio/aac" />
             </video>
           </div>
-          <Modal
-            id="video-modal"
-            show={videoModalActive}
-            handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
-            videoTag="iframe" />
         </div>
       </div>
     </section>
   );
-}
+};
 
 Hero.propTypes = propTypes;
 Hero.defaultProps = defaultProps;
---
+++ src/components/sections/RadioPlayer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const RadioPlayer = () => {
+  return (
+    <div className="">
+      <center>
+        <iframe
+          loading="lazy"
+          class="cuadroBordeado"
+          src="https://cp.usastreams.com/pr2g/APPlayerRadioHTML5.aspx?stream=https://eu1.serviaudio.com:10925/;&amp;fondo=05&amp;formato=aacp&amp;color=14&amp;titulo=2&amp;autoStart=1&amp;vol=10&amp;tipo=7&amp;nombre=Fm++Expres&amp;imagen=http://www.fmexpres.com/wp-content/uploads/2019/08/cropped-logo2.png&amp;server=https://eu1.serviaudio.com:10925/index.htmlCHUMILLASsid=1&amp;opt=metadata"
+          name="contenedorPlayer"
+          width="300px"
+          height="105px"
+          frameborder="0"
+          marginwidth="0"
+          marginheight="0"
+          scrolling="no"
+        ></iframe>
+      </center>
+    </div>
+  );
+};
+
+export default RadioPlayer;
